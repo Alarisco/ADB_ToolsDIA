8bdf845aea78f828ff7c4be6a9c6d88d7b678b91
Alvaro Alonso <alarisco@gmail.com>
2022-04-26 20:05:31 +0200

    update

--- modulos/funcion.sh
+++ modulos/funcion.sh
@@ -40,22 +40,23 @@ option_list () {
 
 banner
 echo -e "\n\n\e[1;4;91mESCOGE UNA OPCION:\e[0m\n\n"
-echo -e "\e[1;93m1.   \e[1;92mMOSTRAR DISPOSITIVOS CONECTADOS                           \e[1;93m17.  \e[1;92mMOVER FICHERO AL TERMINAL"
+echo -e "\e[1;93m1.   \e[1;92mMOSTRAR DISPOSITIVOS CONECTADOS                           \e[1;93m18.  \e[1;92mMOVER FICHERO AL TERMINAL"
 echo -e "\e[1;93m2.   \e[1;92mREINICIAR EL SERVICIO ADB"
 echo -e "\e[1;93m3.   \e[1;92mREINICIAR EL TERMINAL CONECTADO"
-echo -e "\e[1;93m4.   \e[1;92mLANZA UNA CONSOLA ADB_SHELL                               \e[1;93m18.  \e[1;92mEJECUTA UNA APLICACION "
-echo -e "\e[1;93m5.   \e[1;92mINFORMACION DEL TERMINAL"
-echo -e "\e[1;93m6.   \e[1;92mINSTALL AN PACAKAGE 'apk'"
-echo -e "\e[1;93m7.   \e[1;92mDESISTALA UNA APLICACION"
-echo -e "\e[1;93m8.   \e[1;92mLISTA DE TODAS LAS APKS INSTALADAS"
-echo -e "\e[1;93m9.   \e[1;92mTAILOG DEL TERMINAL"
-echo -e "\e[1;93m10.  \e[1;92mREALIZA UNA CAPTURA DE PANTALLA"
-echo -e "\e[1;93m11.  \e[1;92mREALIZA UNA GRABACION DE LA PANTALLA"
-echo -e "\e[1;93m12.  \e[1;92mCOPIA TODAS LAS FOTOS DEL TERMINAL"
-echo -e "\e[1;93m13.  \e[1;92mCOPIA TODA LA CARPETA DOWNLOAD"
-echo -e "\e[1;93m14.  \e[1;92mCOPIA TODA LA CARPETA WHATSAPP"
-echo -e "\e[1;93m15.  \e[1;92mCOPIA COMPLETA DEL TERMINAL ( Takes time )"
-echo -e "\e[1;93m16.  \e[1;92mCOPIA UNA CARPETA ESPECIFICA DEL TERMINAL                  \e[1;91mEXIT o PRESIONA Ctrl+c\n"
+echo -e "\e[1;93m4.   \e[1;92mLANZA UNA CONSOLA ADB_SHELL                               \e[1;93m19.  \e[1;92mEJECUTA UNA APLICACION "
+echo -e "\e[1;93m5.   \e[1;92mINFORMACION DEL TERMINAL COMPLETA"
+echo -e "\e[1;93m6.   \e[1;92mINFORMACION DEL TERMINAL RESUMEN"
+echo -e "\e[1;93m7.   \e[1;92mINSTALA UNA APLICACION 'apk'"
+echo -e "\e[1;93m8.   \e[1;92mDESISTALA UNA APLICACION"
+echo -e "\e[1;93m9.   \e[1;92mLISTA DE TODAS LAS APKS INSTALADAS"
+echo -e "\e[1;93m10.   \e[1;92mTAILOG DEL TERMINAL"
+echo -e "\e[1;93m11.  \e[1;92mREALIZA UNA CAPTURA DE PANTALLA"
+echo -e "\e[1;93m12.  \e[1;92mREALIZA UNA GRABACION DE LA PANTALLA"
+echo -e "\e[1;93m13.  \e[1;92mCOPIA TODAS LAS FOTOS DEL TERMINAL"
+echo -e "\e[1;93m14.  \e[1;92mCOPIA TODA LA CARPETA DOWNLOAD"
+echo -e "\e[1;93m15.  \e[1;92mCOPIA TODA LA CARPETA WHATSAPP"
+echo -e "\e[1;93m16.  \e[1;92mCOPIA COMPLETA DEL TERMINAL ( Takes time )"
+echo -e "\e[1;93m17.  \e[1;92mCOPIA UNA CARPETA ESPECIFICA DEL TERMINAL                  \e[1;91mEXIT o PRESIONA Ctrl+c\n"
 echo -e $revised
 read -p $'\e[1;4;91mINTRODUCE EL NUMERO DE LA OPCION QUE DESEAS EJECUTAR\e[0m\e[24;1;97m : ' options
 }
@@ -75,7 +76,7 @@ case $options in
       "3") echo -e; bash modulos/opt3;;
       "4") echo -e; bash modulos/opt4;;
       "5") echo -e; bash modulos/opt5;;
-
+      "6") echo -e; bash modulos/opt6;;       
 
 
       *) clear; revised="\e ERROR, ENTRADA NO RECONOCIDA\n"; option_list;;
