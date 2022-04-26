11bc73047927ca2c5ae54ae638535a9726d56da9
Alvaro Alonso <alarisco@gmail.com>
2022-04-26 23:39:42 +0200

    update

--- modulos/funcion.sh
+++ modulos/funcion.sh
@@ -54,7 +54,7 @@ echo -e "\e[1;93m13.  \e[1;92mCOPIA TODAS LAS FOTOS DEL TERMINAL"
 echo -e "\e[1;93m14.  \e[1;92mCOPIA TODA LA CARPETA DOWNLOAD"
 echo -e "\e[1;93m15.  \e[1;92mCOPIA TODA LA CARPETA WHATSAPP"
 echo -e "\e[1;93m16.  \e[1;92mCOPIA COMPLETA DEL TERMINAL ( Takes time )"
-echo -e "\e[1;93m17.  \e[1;92mCOPIA UNA CARPETA ESPECIFICA DEL TERMINAL                  \e[1;91mEXIT o PRESIONA Ctrl+c\n"
+echo -e "\e[1;93m17.  \e[1;92mCOPIA UNA CARPETA ESPECIFICA DEL TERMINAL                  \e[1;91mPRESIONA Ctrl+c PARA SALIR\n"
 echo -e $revised
 read -p $'\e[1;4;91mINTRODUCE EL NUMERO DE LA OPCION QUE DESEAS EJECUTAR\e[0m\e[24;1;97m : ' options
 }
