6a616971abb6064b27abbf0469e8098b776c629f
Alvaro Alonso <alarisco@gmail.com>
2022-04-26 23:42:13 +0200

    update

--- ADB_ToolsDIA.sh
+++ ADB_ToolsDIA.sh
@@ -12,7 +12,7 @@ echo -e "Comprobando si hay una nueva versión"
 comprueba_nueva_version () {
 
     if ping -q -c 1 -W 1 google.com >/dev/null; then
-        checked_version=$(curl -s https://github.com/Alarisco/ADB_ToolsDIA/blob/main/modulos/version)
+        checked_version=$(curl -s https://raw.githubusercontent.com/Alarisco/ADB_ToolsDIA/main/modulos/version)
             if ["$checked_version" != "$actualversion"]
                 then
                     echo -e "Versión Actual = $actualversion , Nueva Versión = $checked_version"
