cbd39e87904cdb32d164f913b1b53c48cf5d61c6
Alvaro Alonso <alarisco@gmail.com>
2022-04-27 12:40:41 +0200

    update

--- modulos/funcion.sh
+++ modulos/funcion.sh
@@ -40,21 +40,21 @@ option_list () {
 
 banner
 echo -e "\n\n\e[1;4;91mESCOGE UNA OPCION:\e[0m\n\n"
-echo -e "\e[1;93m1.   \e[1;92mMOSTRAR DISPOSITIVOS CONECTADOS                           \e[1;93m18.  \e[1;92mMOVER FICHERO AL TERMINAL"
+echo -e "\e[1;93m1.   \e[1;92mMOSTRAR DISPOSITIVOS CONECTADOS                           \e[1;93m16.  \e[1;92mMOVER FICHERO AL DISPOSITIVO"
 echo -e "\e[1;93m2.   \e[1;92mREINICIAR EL SERVICIO ADB"
 echo -e "\e[1;93m3.   \e[1;92mREINICIAR EL TERMINAL CONECTADO"
-echo -e "\e[1;93m4.   \e[1;92mLANZA UNA CONSOLA ADB_SHELL                               \e[1;93m19.  \e[1;92mEJECUTA UNA APLICACION "
-echo -e "\e[1;93m5.   \e[1;92mINFORMACION DEL TERMINAL COMPLETA"
-echo -e "\e[1;93m6.   \e[1;92mINFORMACION DEL TERMINAL RESUMEN"
+echo -e "\e[1;93m4.   \e[1;92mLANZA UNA CONSOLA ADB_SHELL                               \e[1;93m17.  \e[1;92mEJECUTA UNA APLICACION "
+echo -e "\e[1;93m5.   \e[1;92mINFORMACION DEL TERMINAL COMPLETA                         \e[1;93m18.  \e[1;92mEJECUTA APP GESTION TIENDA DIA% "
+echo -e "\e[1;93m6.   \e[1;92mINFORMACION DEL TERMINAL RESUMEN                          \e[1;93m19.  \e[1;92mEJECUTA APP QUICK_SUPPORT "
 echo -e "\e[1;93m7.   \e[1;92mINSTALA UNA APLICACION 'apk'"
 echo -e "\e[1;93m8.   \e[1;92mDESINSTALA UNA APLICACION"
 echo -e "\e[1;93m9.   \e[1;92mLISTA DE TODAS LAS APKS INSTALADAS"
 echo -e "\e[1;93m10.  \e[1;92mTAILOG DEL TERMINAL"
-echo -e "\e[1;93m13.  \e[1;92mCOPIA TODAS LAS FOTOS DEL TERMINAL"
-echo -e "\e[1;93m14.  \e[1;92mCOPIA TODA LA CARPETA DOWNLOAD"
-echo -e "\e[1;93m15.  \e[1;92mCOPIA TODA LA CARPETA WHATSAPP"
-echo -e "\e[1;93m16.  \e[1;92mCOPIA COMPLETA DEL TERMINAL ( Takes time )"
-echo -e "\e[1;93m17.  \e[1;92mCOPIA UNA CARPETA ESPECIFICA DEL TERMINAL                  \e[1;91mPRESIONA Ctrl+c PARA SALIR\n"
+echo -e "\e[1;93m11.  \e[1;92mCOPIA TODAS LAS FOTOS DEL TERMINAL"
+echo -e "\e[1;93m12.  \e[1;92mCOPIA TODA LA CARPETA DOWNLOAD"
+echo -e "\e[1;93m13.  \e[1;92mCOPIA TODA LA CARPETA DIA% (Subida a HSR opcional)"
+echo -e "\e[1;93m14.  \e[1;92mCOPIA COMPLETA DEL TERMINAL ( Lleva tiempo )"
+echo -e "\e[1;93m15.  \e[1;92mCOPIA UNA CARPETA ESPECIFICA DEL TERMINAL                  \e[1;91mPRESIONA Ctrl+c PARA SALIR\n"
 echo -e $revised
 read -p $'\e[1;4;91mINTRODUCE EL NUMERO DE LA OPCION QUE DESEAS EJECUTAR\e[0m\e[24;1;97m : ' options
 }
