718878c6838ac2bcdd5a95f9a220a80574574e2c
Alvaro Alonso <alarisco@gmail.com>
2022-04-28 16:49:09 +0200

    add initial clear

--- ADB_ToolsDIA.sh
+++ ADB_ToolsDIA.sh
@@ -3,6 +3,8 @@
 #Author = Alvaro Alonso
 #Date = 2022
 
+clear
+
 current_version=2.0.1
 
 banner () {
@@ -42,7 +44,7 @@ check_new_version () {
             then
                 echo -e "Current Version = $current_version , New Version = $checked_version"
                 echo -e "\n\e[91;7mHay una nueva version disponible\e[27m\n"
-                curl -s https://raw.githubusercontent.com/Alarisco/ADB_ToolsDIA/main/changelog/changelog.txt
+                curl -s https://raw.githubuserAcontent.com/Alarisco/ADB_ToolsDIA/main/changelog/changelog.txt
                 echo -e
                 sleep 5 | echo -e "\e[93;5mEspera 5s o solicita la ultima versión\e[0m"
 
